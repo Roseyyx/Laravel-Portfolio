e82f583b2263e26291f92403f8e202ce17831c48
Rose <arty@duck.com>
2023-06-20 18:20:05 +0200

    delete

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/product', [ProductController::class, 'index'])->name('products.index');	
-Route::get('/product/create', [ProductController::class, 'create'])->name('products.create');	
-Route::post('/product', [ProductController::class, 'store'])->name('products.store');	
