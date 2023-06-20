65a65118b434338d29dae7ceb8287563010a5b61
Rose <arty@duck.com>
2023-06-20 21:58:44 +0200

    vue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import './bootstrap'
+import { createApp} from 'vue/dist/vue.esm-bundler.js'
+
+// register components
+
+const app = createApp({})
+
+// app.component
+
+
+app.mount('#app')
